e51bbb4ff21c8996c00db70a53e7f2b86ea35f5c
Sina <general.sina.amini.20@gmail.com>
2022-09-05 12:37:30 +0430

    category model created and registered in news/admin.py

--- news/models.py
+++ news/models.py
@@ -10,11 +10,20 @@ def new_upload_img_path(instance, filepath):
     path = f'news/{instance.title}/{new_name}{ext}'
     return path
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    def __str__(self) -> str:
+        return self.title
+
 class News(BaseModel):
     title = models.CharField(max_length=3000)
     short_description = models.CharField(max_length=5000)
     description = models.TextField()
     img = models.ImageField(upload_to=new_upload_img_path, null=True, blank=True)
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
